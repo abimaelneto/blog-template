01adcd06d720e389121ecf7816b0cd23761fd660
Abimael Neto <abimael.oliveiraneto@gmail.com>
2023-05-16 10:52:11 -0300

    finaliza criação de posts

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,95 @@
 <script>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from "vue-router";
 
 export default {
-  data(){
+  data() {
     return {
       posts: [
         {
-          title:'Meu Primeiro Post',
+          title: "Meu Primeiro Post",
           datetime: Date.now(),
-          content: 'Postar aqui é muito legal',
-
+          content: "Postar aqui é muito legal",
         },
-           {
-          title:'Meu Segundo Post',
+        {
+          title: "Meu Segundo Post",
           datetime: Date.now(),
-          content: 'Postar aqui não é tão legal',
-            
-        }
-      ]
-    }
+          content: "Postar aqui não é tão legal",
+        },
+      ],
+      formData: {
+        title: "",
+        content: "",
+      },
+    };
   },
   methods: {
-    handleClick(event){
-      event.stopPropagation()
-      // document.querySelector("form").submit()
-    }
-  }
-}
+    handleClick(event) {
+      //adicionar o post à lista de posts
+      const now = new Date();
 
+      const dataDaPostagem = `${now.getDate()}/${
+        now.getMonth() + 1
+      }/${now.getFullYear()}`;
 
+      // //método 1
+      // this.posts[this.posts.length] = {
+      //   title: this.formData.title,
+      //   content: this.formData.content,
+      // };
 
-// for(const algumacoisa in posts) {
-//   //indice
-// }
+      // //método 2
+      this.posts.push({
+        title: this.formData.title,
+        content: this.formData.content,
+        datetime: dataDaPostagem,
+      });
 
-// for(const algumacoisa of posts) {
-//   //valor
-// }
+      this.formData = {
+        title: "",
+        content: "",
+      };
+    },
 
+    handleInputChange(event) {
+      const { name, value } = event.target;
+      this.formData[name] = value;
+
+      // quando estiver executando, fica assim:
+      // this.formData['title'] = "Meu titulo mais legal"
+    },
+  },
+};
 </script>
 
 <template>
-  
-  <div id="lista-posts" >
+  <div id="lista-posts">
     <div class="post" v-for="post in posts" :key="post.key">
-        <h3>{{ post.title }}</h3>
-        <h4>{{ post.datetime}} </h4>
-        <p>{{ post.content}}</p>
+      <h3>{{ post.title }}</h3>
+      <h4>{{ post.datetime }}</h4>
+      <p>{{ post.content }}</p>
     </div>
   </div>
 
+  <form>
+    <input v-model="formData.title" placeholder="Título" />
 
+    <textarea
+      name="content"
+      :value="formData.content"
+      @keyup="handleInputChange"
+      placeholder="Escreva seu post aqui..."
+      id=""
+      cols="30"
+      rows="10"
+    ></textarea>
 
-  <form action="">
-    <input name="title">
-    <textarea name="content" id="" cols="30" rows="10"></textarea>
-
-    <button @click="handleClick">Criar2 </button>
+    <button type="button" @click="handleClick">Salvar</button>
   </form>
 
   <RouterView />
 </template>
 
 <style scoped>
-
 form {
   display: flex;
   flex-direction: column;
@@ -72,5 +98,4 @@ form {
 form > * {
   margin: 1rem;
 }
-
 </style>
