06b7f663e34ce05e2904508b2eacced1433361e1
Abimael Neto <abimael.oliveiraneto@gmail.com>
2023-05-19 14:04:46 -0300

    cria a view de edição

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -1,1 +1,26 @@
-<template></template>
+<script>
+import PostForm from "@/components/PostForm.vue";
+
+export default {
+  props: {
+    posts: Array,
+  },
+  data() {
+    //acessa o parâmetro id uma única vez
+    const id = this.$route.params.id;
+
+    return {
+      //expondo a variável id para ser acessível no template
+      id: id,
+      post: this.posts[id],
+    };
+  },
+  components: {
+    PostForm,
+  },
+};
+</script>
+
+<template>
+  <PostForm :post="post" :id="id" />
+</template>
