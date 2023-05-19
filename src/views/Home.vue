4bffb349ab93591a59a61a297ff803759f468133
Abimael Neto <abimael.oliveiraneto@gmail.com>
2023-05-19 14:06:03 -0300

    redireciona para a view de edição conforme id do post

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,5 @@
 <script>
+import { RouterLink } from "vue-router";
 export default {
   props: {
     posts: Array,
@@ -25,15 +26,30 @@ export default {
       return listaFiltrada;
     },
   },
+  methods: {
+    getPostId(title) {
+      // passa pela lista de posts(não filtrada)
+      for (const index in this.posts) {
+        //acessa o post na posição index da lista de posts
+        const post = this.posts[index];
+        //verifica se o título do post atual é igual ao título buscado
+        if (post.title === title) return index;
+      }
+    },
+  },
 };
 </script>
 
 <template>
-  {{ aFuncaoComputadaMaisLegalDoMundo }}
   <input v-model="search" placeholder="Procure pelo título do post..." />
   <div id="lista-posts">
     <div class="post" v-for="post in filteredPosts" :key="post.key">
-      <h3>{{ post.title }}</h3>
+      <h3>
+        {{ post.title }}
+        <RouterLink :to="`/edit/${getPostId(post.title)}`">
+          <span class="material-symbols-rounded">edit</span>
+        </RouterLink>
+      </h3>
       <h4>{{ post.datetime }}</h4>
       <p>{{ post.content }}</p>
     </div>
