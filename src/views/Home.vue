42d733f39df59ba86851194659d3417a4228c0d2
Abimael Neto <abimael.oliveiraneto@gmail.com>
2023-05-22 11:42:17 -0300

    adiciona deleção

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,6 +7,8 @@ export default {
   data() {
     return {
       search: "",
+      showModal: false,
+      selectedPost: null,
     };
   },
   computed: {
@@ -36,6 +38,26 @@ export default {
         if (post.title === title) return index;
       }
     },
+    setupModal(id) {
+      // mostrar e esconder o modal
+      this.showModal = !this.showModal;
+
+      // selecionar o post e desselecionar o post
+      if (id) {
+        // salva o post inteiro(com título, conteúdo e data)
+        this.selectedPost = this.posts[id];
+        return;
+      }
+
+      this.selectedPost = null;
+    },
+    deletePost() {
+      const id = this.getPostId(this.selectedPost.title);
+      this.$emit("delete-post", id);
+
+      //feche o modal e desselecione o post
+      this.setupModal();
+    },
   },
 };
 </script>
@@ -49,9 +71,26 @@ export default {
         <RouterLink :to="`/edit/${getPostId(post.title)}`">
           <span class="material-symbols-rounded">edit</span>
         </RouterLink>
+        <span
+          class="material-symbols-rounded"
+          @click="setupModal(getPostId(post.title))"
+          >delete</span
+        >
       </h3>
       <h4>{{ post.datetime }}</h4>
       <p>{{ post.content }}</p>
+    </div>
+  </div>
+
+  <div class="modal" v-show="showModal">
+    <div class="modal-content">
+      <h3>Deletar Post</h3>
+      <p>Tem certeza que quer deletar o post '{{ selectedPost?.title }}' ?</p>
+
+      <div class="modal-actions">
+        <button class="bg-error" @click="setupModal">Cancelar</button>
+        <button class="bg-success" @click="deletePost">Confirmar</button>
+      </div>
     </div>
   </div>
 </template>
